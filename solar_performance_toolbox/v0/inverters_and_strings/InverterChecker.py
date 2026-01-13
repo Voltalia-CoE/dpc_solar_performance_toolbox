38b3abd8b18c01b20f4fb30e5d3310bf2e4f7310
Jonas Gusmão <j.gusmao@voltalia.com>
2026-01-13 17:03:03 -0300

    enhancement: some upgrades on the general codes

--- solar_performance_toolbox/v0/inverters_and_strings/InverterChecker.py
+++ solar_performance_toolbox/v0/inverters_and_strings/InverterChecker.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 class InverterChecker:
 
-    def __init__(self, agg_func:str = "median"):
+    def __init__(self, agg_func: str = "median"):
         self.agg_func = agg_func
 
     def calculate_production_deltas(
@@ -14,14 +14,14 @@ class InverterChecker:
         resample: None | str = None,
         agg_func: None | str = "mean",
         *args,
-        **kwargs
+        **kwargs,
     ):
 
         data0 = data.copy()
 
         if resample is not None:
             data0 = data0.resample(resample).agg(agg_func)
-        
+
         match one_to_x:
             case "one":
                 return self._calculate_deltas_one_to_one(target_eqp, data0)
@@ -30,20 +30,21 @@ class InverterChecker:
             case _:
                 NotImplementedError()
 
-    
-    def _calculate_deltas_one_to_one(self, target_eqp:str, data:pd.DataFrame):
+    def _calculate_deltas_one_to_one(self, target_eqp: str, data: pd.DataFrame):
 
         data_new = data.copy()
         for eqp in data.columns:
 
             data_new[eqp] = ((data[eqp] / data[target_eqp]) - 1) * 100
 
+        data_new["REFERENCE_EQP"] = target_eqp
+
         return data_new
-    
-    def _calculate_deltas_one_to_rest(self, target_eqp:str, data:pd.DataFrame):
+
+    def _calculate_deltas_one_to_rest(self, target_eqp: str, data: pd.DataFrame):
 
         data_new = data.copy()
-        
+
         equips_to_compare = list(data.columns).copy()
         equips_to_compare.remove(target_eqp)
 
@@ -51,25 +52,75 @@ class InverterChecker:
 
         data_new = ((data_new[target_eqp] / others_data) - 1) * 100
 
+        data_new = data_new.rename(target_eqp)
+
         return data_new
 
-    def calculate_availability(self, inv_df:pd.DataFrame, irrad_df:pd.DataFrame):
+    def calculate_availability(
+        self,
+        inv_data: pd.DataFrame | pd.Series,
+        irrad_data: pd.DataFrame,
+        interval: str = "D",
+    ):
 
-        data2 = inv_df.copy()
-        data2 = data2.reindex(irrad_df.index)
-        data2:pd.DataFrame = data2.loc[irrad_df > 50]
+        data2 = inv_data.copy()
+        data2 = data2.reindex(irrad_data.index)
+        data2 = data2.loc[irrad_data > 50]
 
-        data3 = data2.loc[(data2 > 1000)].dropna() # | (data2.isna())
+        if isinstance(data2, pd.Series):
+            return self._calculate_time_availability_series(data2, interval)
+        
+        elif isinstance(data2, pd.DataFrame):
+            return self._calculate_time_availability_df(data2, interval)
+    
+    def _calculate_time_availability_series(self, inv_srs:pd.Series, interval:str):
 
-        time_avail = len(data3) / len(data2)
+        valid = (inv_srs > 1000)
+
+        if interval == "D":
+            time_avail = (
+                valid
+                .resample("D")
+                .mean()
+                .rename("VALUE")
+                # .reset_index(name="TS")
+            )
+
+        elif interval == "all":
+            time_avail = valid.mean()
 
         return time_avail
-    
-    def calculate_efficiency(self, ac_df:pd.DataFrame, dc_df:pd.DataFrame):
 
-        eff_df = ac_df / dc_df
+    def _calculate_time_availability_df(self, inv_df:pd.Series, interval:str):
 
-        eff_df = eff_df.mean()
+        valid = inv_df > 1000
+
+        if interval == "D":
+            time_avail = (
+                valid
+                .resample("D")
+                .mean()
+                .reset_index()
+            )
+
+        elif interval == "all":
+            time_avail = valid.mean()
+
+        return time_avail
+
+    def calculate_efficiency(self, ac_df: pd.DataFrame, dc_df: pd.DataFrame, interval: str = "D"):
+
+        eff = ac_df / dc_df
+
+        if interval == "D":
+            eff_df = (
+                eff
+                .resample("D")
+                .mean()
+                .reset_index()
+            )
+
+        elif interval == "all":
+            eff_df = eff.mean()
 
         return eff_df
-    
--- solar_performance_toolbox/v0/trackers/TrackerChecker.py
+++ solar_performance_toolbox/v0/trackers/TrackerChecker.py
@@ -5,12 +5,24 @@ class TrackerChecker:
     def __init__(self):
         pass
 
-    def get_angle_mae(self, df_ang:pd.DataFrame, df_setpoint:pd.DataFrame):
+    def get_angle_mae(
+        self,
+        df_ang: pd.DataFrame,
+        df_setpoint: pd.DataFrame,
+        interval: str = "D",
+    ):
 
-        df_error = df_setpoint - df_ang   
+        abs_error = (df_setpoint - df_ang).abs()
 
-        df_abs_error = df_error.abs()
+        if interval == "D":
+            df_mae = (
+                abs_error
+                .resample("D")
+                .mean()
+                .reset_index()
+            )
 
-        df_mae = df_abs_error.mean()
+        elif interval == "all":
+            df_mae = abs_error.mean()
 
         return df_mae
